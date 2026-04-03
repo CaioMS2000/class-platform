a180dd87198a39edd5ed085879917ec91fde755a
Caio Marques Silva <caiocpe@gmail.com>
2026-04-02 21:29:20 -0300

    refactor(catalog): introduce base application error class for catalog module
    All catalog application errors now extend CatalogModuleApplicationError,
    enabling instanceof checks across the error hierarchy.

--- apps/main-api/src/modules/catalog/application/@errors/index.ts
+++ apps/main-api/src/modules/catalog/application/@errors/index.ts
@@ -1,32 +1,34 @@
-export class CourseNotFoundError extends Error {
+import { CatalogModuleApplicationError } from './app-error'
+
+export class CourseNotFoundError extends CatalogModuleApplicationError {
 	constructor(public message: string = 'Course not found') {
 		super(message)
 		this.name = 'CourseNotFoundError'
 	}
 }
 
-export class ModuleNotFoundError extends Error {
+export class ModuleNotFoundError extends CatalogModuleApplicationError {
 	constructor(public message: string = 'Module not found') {
 		super(message)
 		this.name = 'ModuleNotFoundError'
 	}
 }
 
-export class LessonNotFoundError extends Error {
+export class LessonNotFoundError extends CatalogModuleApplicationError {
 	constructor(public message: string = 'Lesson not found') {
 		super(message)
 		this.name = 'LessonNotFoundError'
 	}
 }
 
-export class CategoryNotFoundError extends Error {
+export class CategoryNotFoundError extends CatalogModuleApplicationError {
 	constructor(public message: string = 'Category not found') {
 		super(message)
 		this.name = 'CategoryNotFoundError'
 	}
 }
 
-export class NotCourseOwnerError extends Error {
+export class NotCourseOwnerError extends CatalogModuleApplicationError {
 	constructor(
 		public message: string = 'Instructor is not the owner of this course'
 	) {
